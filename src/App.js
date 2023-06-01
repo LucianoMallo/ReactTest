5d8162c89141ef2a80ef0ff7b3e36e372202cf0f
Luciano Ezequiel Mallo Tomaso <malloluciano@gmail.com>
2023-06-01 09:33:23 +0200

    initial commit
    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+      <footer>
+        Home | Terms and Conditions | Privacy Policies | Collections Statement | Help | Manage Account</footer>
+    </div>
+  );
+}
+
+export default App;
